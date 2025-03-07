33e52058a58324013e0e568371cb82b413d6c178
ahmedali109 <ahmedalinaguib33@gmail.com>
2025-03-08 01:51:42 +0200

    add sliding text animation

--- lib/features/splach/presentation/view/widget/splach_view_body.dart
+++ lib/features/splach/presentation/view/widget/splach_view_body.dart
@@ -1,25 +1,47 @@
 import 'package:bookly_app/core/util/constant/assets.dart';
+import 'package:bookly_app/features/splach/presentation/view/widget/sliding_text.dart';
 import 'package:flutter/material.dart';
 
-class SplachViewBody extends StatelessWidget {
+class SplachViewBody extends StatefulWidget {
   const SplachViewBody({super.key});
+
+  @override
+  State<SplachViewBody> createState() => _SplachViewBodyState();
+}
+
+class _SplachViewBodyState extends State<SplachViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+  @override
+  void initState() {
+    super.initState();
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    );
+    slidingAnimation = Tween<Offset>(
+      begin: const Offset(0, 2),
+      end: const Offset(0, 0),
+    ).animate(animationController);
+    animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.stretch,
-      spacing: 8,
+      spacing: 5,
       children: [
         Image.asset(AssetsData.logo),
-        const Text(
-          "Read Free Books",
-          textAlign: TextAlign.center,
-          style: TextStyle(
-            fontSize: 22,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
+        SlidingText(slidingAnimation: slidingAnimation),
       ],
     );
   }
