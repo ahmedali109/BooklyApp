ee9ffefeb1beb28512bb46533c41e0d52622f262
ahmedali109 <ahmedalinaguib33@gmail.com>
2025-03-08 02:41:59 +0200

    add custom app bar to home

---
+++ lib/features/splach/presentation/view/widget/custom_app_bar.dart
@@ -0,0 +1,32 @@
+import 'package:bookly_app/core/util/constant/assets.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Image.asset(
+              AssetsData.logo,
+              height: 20,
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                FontAwesomeIcons.magnifyingGlass,
+                size: 24,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
