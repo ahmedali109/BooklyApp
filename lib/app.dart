9ba3159cfeb46a24281e12d56af4b714df98fb73
ahmedali109 <ahmedalinaguib33@gmail.com>
2025-03-09 21:37:42 +0200

    creates book details and refactor the files to work perfectly

--- lib/app.dart
+++ lib/app.dart
@@ -2,7 +2,6 @@ import 'package:bookly_app/core/util/constant/colors.dart';
 import 'package:bookly_app/features/splach/presentation/view/splach_view.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 class BooklyApp extends StatelessWidget {
   const BooklyApp({super.key});
@@ -16,7 +15,6 @@ class BooklyApp extends StatelessWidget {
         brightness: Brightness.dark,
         primarySwatch: Colors.blue,
         useMaterial3: false,
-        textTheme: GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
       ),
       home: const SplachView(),
     );
