dca647fff16ab6d157b82effd746cc2712d2d437
ahmedali109 <ahmedalinaguib33@gmail.com>
2025-03-11 00:44:28 +0200

    implement fetchNewestBooks

--- lib/core/util/service/api_service.dart
+++ lib/core/util/service/api_service.dart
@@ -2,11 +2,11 @@ import 'package:dio/dio.dart';
 
 class ApiService {
   final _baseurl = "https://www.googleapis.com/books/v1/";
-  final Dio dio;
-  ApiService(this.dio);
+  final Dio _dio;
+  ApiService(this._dio);
 
-  Future<Map<String, dynamic>> get(String endpoint) async {
-    final response = await dio.get(_baseurl + endpoint);
+  Future<Map<String, dynamic>> get({required String endpoint}) async {
+    final response = await _dio.get(_baseurl + endpoint);
     return response.data;
   }
 }
--- lib/features/home/data/repo/home_repo_impl.dart
+++ lib/features/home/data/repo/home_repo_impl.dart
@@ -1,20 +1,29 @@
 import 'package:bookly_app/core/error/failure.dart';
+import 'package:bookly_app/core/util/service/api_service.dart';
 import 'package:bookly_app/features/home/data/model/book_model/book_model.dart';
 import 'package:bookly_app/features/home/data/repo/home_repo.dart';
 import 'package:dartz/dartz.dart';
 
 class HomeRepoImpl implements HomeRepo {
+  final ApiService apiService;
+  HomeRepoImpl(this.apiService);
   @override
-  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks() {
-    // TODO: implement fetchBestSellerBooks
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModel>>> fetchNewestBooks() async {
+    try {
+      var data = await apiService.get(endpoint: "volumes?Filtering=free-ebooks&Sorting=newest&q=computer science");
+       List<BookModel> books = [];
+      for (var item in data['items']) {
+        books.add(BookModel.fromJson(item));
+      }
+      return right(books);
+    } catch (e) {
+      return left(ServerFailure());
+    }
   }
 
   @override
   Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() {
-    // TODO: implement fetchFeaturedBooks
+    // apiService.get(endpoint: "volumes?Filtering=free-ebooks&q=subject:Programming");
     throw UnimplementedError();
   }
-
-
 }
