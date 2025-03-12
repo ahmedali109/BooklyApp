9cd6cd0f6c138c2cf5fa0f152eae849025ac2a71
ahmedali109 <ahmedalinaguib33@gmail.com>
2025-03-12 17:31:28 +0200

    create newest cubit methods

--- lib/features/home/presentation/manger/newest_news/newest_news_cubit.dart
+++ lib/features/home/presentation/manger/newest_news/newest_news_cubit.dart
@@ -1,9 +1,19 @@
 import 'package:bloc/bloc.dart';
 import 'package:bookly_app/features/home/data/model/book_model/book_model.dart';
+import 'package:bookly_app/features/home/data/repo/home_repo.dart';
 import 'package:equatable/equatable.dart';
 
 part 'newest_news_state.dart';
 
 class NewsetNewsCubit extends Cubit<NewsetNewsState> {
-  NewsetNewsCubit() : super(NewsetNewsInitial());
+  NewsetNewsCubit(this.homeRepo) : super(NewsetNewsInitial());
+  final HomeRepo homeRepo;
+  Future<void> fetchNewestBooks() async {
+    emit(NewsetNewsLoading());
+    final result = await homeRepo.fetchNewestBooks();
+    result.fold(
+      (failure) => emit(NewsetNewsFailure(failure.message)),
+      (books) => emit(NewsetNewsSuccess(books)),
+    );
+  }
 }
