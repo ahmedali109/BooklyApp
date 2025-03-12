2167697b2aae6f3c5b7089aaba3f767c331433e9
ahmedali109 <ahmedalinaguib33@gmail.com>
2025-03-12 19:50:33 +0200

    setupServiceLocator

--- lib/app.dart
+++ lib/app.dart
@@ -1,20 +1,35 @@
 import 'package:bookly_app/core/util/constant/colors.dart';
 import 'package:bookly_app/core/util/routes/routes.dart';
+import 'package:bookly_app/core/util/service/service_locator.dart';
+import 'package:bookly_app/features/home/data/repo/home_repo_impl.dart';
+import 'package:bookly_app/features/home/presentation/manger/featured_books/featured_books_cubit.dart';
+import 'package:bookly_app/features/home/presentation/manger/newest_news/newest_news_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class BooklyApp extends StatelessWidget {
   const BooklyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: AppRoutes.route,
-      title: 'Bookly',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        scaffoldBackgroundColor: kPrimaryColor,
-        brightness: Brightness.dark,
-        primarySwatch: Colors.blue,
-        useMaterial3: false,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => FeaturedBooksCubit(getIt.get<HomeRepoImpl>()),
+        ),
+        BlocProvider(
+          create: (context) => NewsetNewsCubit(getIt.get<HomeRepoImpl>()),
+        ),
+      ],
+      child: MaterialApp.router(
+        routerConfig: AppRoutes.route,
+        title: 'Bookly',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          scaffoldBackgroundColor: kPrimaryColor,
+          brightness: Brightness.dark,
+          primarySwatch: Colors.blue,
+          useMaterial3: false,
+        ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:bookly_app/app.dart';
+import 'package:bookly_app/core/util/service/service_locator.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+  setupServiceLocator();
   runApp(const BooklyApp());
 }
